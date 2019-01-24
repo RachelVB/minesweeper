7bee094b40daea4bd088764accaa9968c41a5e55
RachelVB <vanbuskirk2014@hotmail.com>
2019-01-23 21:47:27 -0800

    step 4 minesweeper

--- minesweeper.js
+++ minesweeper.js
@@ -41,7 +41,7 @@ printBoard(board);
 const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
   const board = [];
   for(let i = 0; i < numberOfRows; i++) {
-    // I used the = sign in my condition and I received a FATAL ERROR in my termianl. 
+    // I used the = sign in my loop condition and I received a FATAL ERROR in my termianl. 
     let row = [];
     for(let j = 0; j < numberOfColumns; j++) {
       row.push(' ');
@@ -64,19 +64,55 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 while (numberOfBombsPlaced < numberOfBombs){
 /* The code in the while loop has the potential to place bombs 
 on top of already existing bombs. This will be fixed when you learn about control flow.*/
-const randomRowIndex = Math.floor(Math.random() * numberOfRows);
-const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+
+if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+board[randomRowIndex][randomColumnIndex] = 'B';
+numberOfBombsPlaced++
+}
 
 board[randomRowIndex][randomColumnIndex] = 'B';
-
 numberOfBombsPlaced++
 }
   return board;
 };
 
+const getNumberOfNeighborBombs = (bombBoard,rowIndex,columnIndex) => {
+const neighborOffsets = [[-1,-1],[-1,0],[-1,1],[0,-1],[1,-1],[0,1],[1,0],[1,1]];
+
+const numberOfRows = bombBoard.length;
+const numberOfColumns = bombBoard[0].length;
+let numberOfBombs = 0;
+
+//This function below is used to retrun the number of bombs in an adjacent neighbor.
+neighborOffsets.forEach(offset => {
+const neighborRowIndex = rowIndex + offset[0];
+const neighborColumnIndex = columnIndex + offset[1];
+  if(neighborRowIndex >= 0 && neighborRowIndex < numberOfRows && 
+    neighborColumnIndex >= 0 && neighborColumnIndex < numberOfColumns) {
+    if(bombBoard[neighborRowIndex][neighborColumnIndex] === 'B') {
+      numberOfBombs++
+    }
+  }
+});
+return numberOfBombs;
+};
+
+const flipTile = (playerBoard,bombBoard,rowIndex,columnIndex) => {
+if (playerBoard[rowIndex][columnIndex] !== ' ') {
+  console.log('This tile has already been flipped!');
+  return;
+} else if (bombBoard[rowIndex][columnIndex] === 'B') {
+  playerBoard[rowIndex][columnIndex] = 'B';
+} else {
+  playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard,rowIndex,columnIndex);
+}
+};
+
 const printBoard = board => {
   // I used a bracket after my 'row=>' and my table did not print. 
-  console.log(board.map(row => row.join(' | ')).join('\n'))
+  console.log(board.map(row => row.join(' | ')).join('\n'));
 };
 
 const playerBoard = generatePlayerBoard(3,4);
@@ -86,3 +122,7 @@ console.log('Player Board: ');
 printBoard(playerBoard);
 console.log('Bomb Board: ');
 printBoard(bombBoard);
+
+flipTile(playerBoard,bombBoard, 0,1);
+console.log('Updated Player Board: ');
+printBoard(playerBoard);
